54370d9d58537f2412b91f9fb8b4587425c7a425
Ignacio Isturiz <ignacioisturizr@gmail.com>
2025-07-03 17:11:32 -0500

    first commit testing cursor

---
+++ hola_cursor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def main():
+    print("¡Hola desde Cursor! Estoy probando lazygit")
+    print("Este es un archivo de prueba para verificar el funcionamiento de lazygit")
+
+if __name__ == "__main__":
+    main() 
